a5de18ca02c6a92fe42a5263eac112ce103c85a2
DmitaN <dmitan@bk.ru>
2020-08-24 17:39:55 +0300

    ss

--- project/quizz/serializers.py
+++ project/quizz/serializers.py
@@ -12,5 +12,23 @@ class SurveySerializer(serializers.ModelSerializer):
         return active_survey
 
 
+class ResultSurvey(serializers.Serializer):
+    results = serializers.JSONField()
 
+    def validate_results(self, results):
+        if not results:
+            raise serializers.Validationerror("Results must be not null.")
+        return results
+
+    def save(self):
+        results = self.data['results']
+        user = self.context.userId
+        for question_id in results:
+            question = Question.objects.get(pk=question_id)
+            choices = results[question_id]
+            for answer_id in choices:
+                choice = Choice.objects.get(pk=answer_id)
+                ResultSurvey(userId=user, question=question, choice=choice).save()
+                user.is_answer = True
+                user.save()
 # Create your views here.
